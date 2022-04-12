234b12a25b4d6c579006f7bdf2a867ba72fcb793
TuffenDuffen <reg_i@wext.se>
2022-04-12 11:17:52 +0200

    removed .gitignore in pkg folder

---
+++ frontend/WebCalculator/src/app/pkg/wasm_test_bg.js
@@ -0,0 +1,39 @@
+import * as wasm from './wasm_test_bg.wasm';
+
+const lTextDecoder = typeof TextDecoder === 'undefined' ? (0, module.require)('util').TextDecoder : TextDecoder;
+
+let cachedTextDecoder = new lTextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+cachedTextDecoder.decode();
+
+let cachegetUint8Memory0 = null;
+function getUint8Memory0() {
+    if (cachegetUint8Memory0 === null || cachegetUint8Memory0.buffer !== wasm.memory.buffer) {
+        cachegetUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachegetUint8Memory0;
+}
+
+function getStringFromWasm0(ptr, len) {
+    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+}
+/**
+*/
+export function greet() {
+    wasm.greet();
+}
+
+/**
+* @param {number} a
+* @param {number} b
+* @returns {number}
+*/
+export function add(a, b) {
+    const ret = wasm.add(a, b);
+    return ret;
+}
+
+export function __wbg_alert_9d6b878832276689(arg0, arg1) {
+    alert(getStringFromWasm0(arg0, arg1));
+};
+
